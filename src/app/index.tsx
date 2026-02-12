f9e27a78dc18828f467d93215ffefacef8891ab7
updalla-apshir <updallaapshir@gmail.com>
2026-02-12 20:29:04 +0300

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import "./global.css";
+import { Text, View } from "react-native";
+import Greet from "../components/Greet";
+
+export default function App() {
+  return (
+    <View>
+      <Greet />
+    </View>
+  );
+}
---
+++ src/components/Greet.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white mt-10">
+      <Text className="text-2xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
