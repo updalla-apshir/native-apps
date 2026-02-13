7e94831d1e339d2d42b01e5dc94d16ceb1743954
updalla-apshir <updallaapshir@gmail.com>
2026-02-13 22:22:26 +0300

    initial comiit

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white mt-10">
       <Text className="text-2xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Welcome to Nativewind
       </Text>
     </View>
   );
